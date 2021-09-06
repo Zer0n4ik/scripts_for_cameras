18be7106778b3b4fb529758ac1119211d47ae878
Oleg Gataullin <cs.oleg96@gmail.com>
2021-09-06 16:39:58 +0300

    fix script changeHTTP_PORT for XIXIN cams

--- XI_XIN/changeHTTP_PORT.py
+++ XI_XIN/changeHTTP_PORT.py
@@ -3,23 +3,11 @@ import os
 
 import time
 
-ips = []
-#f = open('ipsHI.txt', 'r').read().splitlines()
-#print(f)\
-#for ip in f:
- #   ips.append(ip)
- #   print(ip)
-#print(f)
-#ips = []
-#ips.append(ip)
-b = 4
-aaa = "192.168.1."
-#print(ips)
+
+network_into = "192.168.1."
 for i in range(6,250):
-    b = b +1
     print(i)
-    print(aaa + str(b)) 
 
 #    print(i)
-    os.system(f"curl -d 'cmd=setnetattr&cururl=http%3A%2F%2F192.168.1.{i}%2Fweb%2Fnetwork.html&-ipaddr=192.168.1.{i}&-netmask=255.255.255.0&-gateway=192.168.1.1&-dhcp=off&-dnsstat=0&-fdnsip=192.168.1.1&-sdnsip=%22%22&cmd=setrtspauth&-rtsp_aenable=0&cmd=setrtspport&-rtspport=554&cmd=setrtmpattr&-rtmpport=1935&cmd=sethttpport&-httpport=8032' --user admin:admin http://192.168.1.{i}/web/cgi-bin/hi3510/param.cgi")
+    os.system(f"curl -d 'cmd=setnetattr&cururl=http%3A%2F%2F{network_into}{i}%2Fweb%2Fnetwork.html&-ipaddr={network_into}{i}&-netmask=255.255.255.0&-gateway={network_into}1&-dhcp=off&-dnsstat=0&-fdnsip={network_into}1&-sdnsip=%22%22&cmd=setrtspauth&-rtsp_aenable=0&cmd=setrtspport&-rtspport=554&cmd=setrtmpattr&-rtmpport=1935&cmd=sethttpport&-httpport=8032' --user admin:admin http://{network_into}{i}/web/cgi-bin/hi3510/param.cgi")
    # print(f"curl -d 'cmd=setnetattr&cururl=http%3A%2F%2F{i}%2Fweb%2Fnetwork.html&-ipaddr={i}&-netmask=255.255.255.0&-gateway=192.168.1.1&-dhcp=off&-dnsstat=0&-fdnsip=192.168.1.1&-sdnsip=%22%22&cmd=setrtspauth&-rtsp_aenable=0&cmd=setrtspport&-rtspport=554&cmd=setrtmpattr&-rtmpport=1935&cmd=sethttpport&-httpport=8032' --user admin:admin http://{i}/web/cgi-bin/hi3510/param.cgi")
